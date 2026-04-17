3ea269f5fc162158348b72730588f451e47f434e
qhoa <meoquynhhoa1610@gmail.com>
2026-04-17 15:17:12 +0700

    hoa2

---
+++ QLKHACHSAN/DAL/DBConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKHACHSAN.DAL
+{
+    public class DBConnection
+    {
+        private string connectionString =
+            @"Server=.\SQLEXPRESS;Database=QuanLyKhachSan;Trusted_Connection=True;TrustServerCertificate=True";
+
+        // Mở kết nối
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        // Execute SELECT (trả về DataTable)
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+
+            return dt;
+        }
+
+        // Execute INSERT, UPDATE, DELETE
+        public int ExecuteNonQuery(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Execute scalar (lấy 1 giá trị)
+        public object ExecuteScalar(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return cmd.ExecuteScalar();
+            }
+        }
+    }
+}
