1476edc1ad48adb29acea81cfa7d06e220c8960d
qhoa <meoquynhhoa1610@gmail.com>
2026-04-19 20:39:48 +0700

    hoa3

--- QLKHACHSAN/UI/KhachHangForm.Designer.cs
+++ QLKHACHSAN/UI/KhachHangForm.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvKH = new System.Windows.Forms.DataGridView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtSDTKH = new System.Windows.Forms.TextBox();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTimKH = new System.Windows.Forms.TextBox();
+            this.txtCCCDKH = new System.Windows.Forms.TextBox();
+            this.btnTimKH = new System.Windows.Forms.Button();
+            this.btnSuaKH = new System.Windows.Forms.Button();
+            this.btnRefreshKH = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKH)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvKH
+            // 
+            this.dgvKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKH.Location = new System.Drawing.Point(363, 108);
+            this.dgvKH.Name = "dgvKH";
+            this.dgvKH.RowHeadersWidth = 51;
+            this.dgvKH.RowTemplate.Height = 24;
+            this.dgvKH.Size = new System.Drawing.Size(1457, 686);
+            this.dgvKH.TabIndex = 9;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnRefreshKH);
+            this.groupBox4.Controls.Add(this.btnSuaKH);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(14, 710);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(304, 84);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Chức Năng";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtCCCDKH);
+            this.groupBox3.Controls.Add(this.txtSDTKH);
+            this.groupBox3.Controls.Add(this.txtTenKH);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(14, 294);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(304, 397);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tài Khoản";
+            // 
+            // txtSDTKH
+            // 
+            this.txtSDTKH.Location = new System.Drawing.Point(22, 217);
+            this.txtSDTKH.Name = "txtSDTKH";
+            this.txtSDTKH.Size = new System.Drawing.Size(259, 30);
+            this.txtSDTKH.TabIndex = 2;
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.Location = new System.Drawing.Point(22, 118);
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(259, 30);
+            this.txtTenKH.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnTimKH);
+            this.groupBox1.Controls.Add(this.txtTimKH);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(14, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 171);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tìm Kiếm ";
+            // 
+            // txtTimKH
+            // 
+            this.txtTimKH.Location = new System.Drawing.Point(22, 78);
+            this.txtTimKH.Name = "txtTimKH";
+            this.txtTimKH.Size = new System.Drawing.Size(259, 30);
+            this.txtTimKH.TabIndex = 0;
+            // 
+            // txtCCCDKH
+            // 
+            this.txtCCCDKH.Location = new System.Drawing.Point(22, 323);
+            this.txtCCCDKH.Name = "txtCCCDKH";
+            this.txtCCCDKH.Size = new System.Drawing.Size(259, 30);
+            this.txtCCCDKH.TabIndex = 3;
+            // 
+            // btnTimKH
+            // 
+            this.btnTimKH.BackColor = System.Drawing.Color.Navy;
+            this.btnTimKH.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTimKH.Location = new System.Drawing.Point(79, 130);
+            this.btnTimKH.Name = "btnTimKH";
+            this.btnTimKH.Size = new System.Drawing.Size(118, 35);
+            this.btnTimKH.TabIndex = 10;
+            this.btnTimKH.Text = "Tìm Kiếm";
+            this.btnTimKH.UseVisualStyleBackColor = false;
+            // 
+            // btnSuaKH
+            // 
+            this.btnSuaKH.BackColor = System.Drawing.Color.Navy;
+            this.btnSuaKH.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSuaKH.Location = new System.Drawing.Point(22, 29);
+            this.btnSuaKH.Name = "btnSuaKH";
+            this.btnSuaKH.Size = new System.Drawing.Size(118, 35);
+            this.btnSuaKH.TabIndex = 11;
+            this.btnSuaKH.Text = "Sửa";
+            this.btnSuaKH.UseVisualStyleBackColor = false;
+            // 
+            // btnRefreshKH
+            // 
+            this.btnRefreshKH.BackColor = System.Drawing.Color.Navy;
+            this.btnRefreshKH.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRefreshKH.Location = new System.Drawing.Point(163, 29);
+            this.btnRefreshKH.Name = "btnRefreshKH";
+            this.btnRefreshKH.Size = new System.Drawing.Size(118, 35);
+            this.btnRefreshKH.TabIndex = 11;
+            this.btnRefreshKH.Text = "Refresh";
+            this.btnRefreshKH.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tìm Theo Tên / SĐT : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Tên Khách Hàng :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "SĐT : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CCCD : ";
+            // 
+            // KhachHangForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1835, 891);
+            this.Controls.Add(this.dgvKH);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "KhachHangForm";
             this.Text = "KhachHangForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKH)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvKH;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtSDTKH;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTimKH;
+        private System.Windows.Forms.Button btnSuaKH;
+        private System.Windows.Forms.TextBox txtCCCDKH;
+        private System.Windows.Forms.Button btnTimKH;
+        private System.Windows.Forms.Button btnRefreshKH;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
